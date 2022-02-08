7d098a21b5e72344ed28ad3fbc0ca164eb8661a7
Youssef <binwunderbar@gmail.com>
2022-02-08 12:58:44 +0100

    deleted factura

--- src/clases/Factura.java
+++ src/clases/Factura.java
@@ -3,72 +3,65 @@ package clases;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Factura extends Pendiente
-{
+public class Factura extends Pendiente {
 	private static final long serialVersionUID = 6717239572208402072L;
-	
+
 	// ===== propiedades =====
 	private double costeReparaciones = 0;
 	private double costeMateriales = 0;
 	private double subtotal;
 	private double iva;
 	private double total;
-	
-	// ===== constructores =====
-	// --- por defecto ---
-	public Factura()
-	{
+
+	/**
+	 * constructor por defeccto hereda 
+	 */
+	public Factura() {
 		super();
-		
+
 		costeReparaciones = 0;
 		costeMateriales = 0;
 		subtotal = 0;
 		iva = 0;
 		total = 0;
 	}
-	
+
 	// --- copia ---
-	public Factura(Factura other)
-	{
+	public Factura(Factura other) {
 		super(other);
-		
+
 		this.costeReparaciones = other.costeReparaciones;
 		this.costeMateriales = other.costeMateriales;
 		this.subtotal = other.subtotal;
 		this.iva = other.iva;
 		this.total = other.total;
 	}
-	
+
 	// --- personalizados ---
-	public Factura(String com, Cliente c, Vehiculo v, Cuenta atc, ArrayList<Reparacion> r)
-	{
+	public Factura(String com, Cliente c, Vehiculo v, Cuenta atc, ArrayList<Reparacion> r) {
 		super(com, c, v, atc, r);
-		
+
 		calcularTotal();
 	}
-	
-	public Factura(Pendiente pendiente)
-	{
+
+	public Factura(Pendiente pendiente) {
 		super(pendiente);
-		
+
 		generarCodigo();
 		calcularTotal();
 	}
 
 	// ===== métodos =====
 	// --- personalizado ---
-	private void calcularTotal()
-	{
-		for (Reparacion r : getReparaciones())
-		{
-			for (MaterialUsado mu : r.getMaterialesUsados())
-			{
+	private void calcularTotal() {
+		for (Reparacion r : getReparaciones()) {
+			for (MaterialUsado mu : r.getMaterialesUsados()) {
 				costeMateriales += mu.getCantidad() * mu.getPrecio();
 			}
-			
+
 			costeReparaciones += r.getHoras() * r.getManoObra();
 		}
-		
+
 		subtotal = costeReparaciones + costeMateriales;
 		iva = subtotal * 0.21;
 		total = subtotal + iva;
@@ -76,20 +69,14 @@ public class Factura extends Pendiente
 
 	// --- salida ---
 	@Override
-	public String toString()
-	{
-		return super.toString() +
-				", total reparaciones: " + costeReparaciones +
-				", total materiales: " + costeMateriales +
-				", subtotal: " + subtotal + 
-				", coste iva: " + iva +
-				", total: " + total;
+	public String toString() {
+		return super.toString() + ", total reparaciones: " + costeReparaciones + ", total materiales: "
+				+ costeMateriales + ", subtotal: " + subtotal + ", coste iva: " + iva + ", total: " + total;
 	}
 
 	// --- comparación ---
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
 		result = prime * result + Objects.hash(costeMateriales, costeReparaciones, iva, subtotal, total);
@@ -97,8 +84,7 @@ public class Factura extends Pendiente
 	}
 
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (!super.equals(obj))
@@ -113,54 +99,28 @@ public class Factura extends Pendiente
 				&& Double.doubleToLongBits(total) == Double.doubleToLongBits(other.total);
 	}
 
-	// --- getters y setters ---
-	public double getCosteReparaciones()
-	{
+	/**
+	 * 
+	 * @return devuelve costeReparaciones
+	 */
+	public double getCosteReparaciones() {
 		return costeReparaciones;
 	}
 
-	public void setCosteReparaciones(double costeReparaciones)
-	{
-		this.costeReparaciones = costeReparaciones;
-	}
-
-	public double getCosteMateriales()
-	{
+	public double getCosteMateriales() {
 		return costeMateriales;
 	}
 
-	public void setCosteMateriales(double costeMateriales)
-	{
-		this.costeMateriales = costeMateriales;
-	}
-
-	public double getSubtotal()
-	{
+	public double getSubtotal() {
 		return subtotal;
 	}
 
-	public void setSubtotal(double subtotal)
-	{
-		this.subtotal = subtotal;
-	}
-
-	public double getIva()
-	{
+	public double getIva() {
 		return iva;
 	}
 
-	public void setIva(double iva)
-	{
-		this.iva = iva;
-	}
-
-	public double getTotal()
-	{
+	public double getTotal() {
 		return total;
 	}
 
-	public void setTotal(double total)
-	{
-		this.total = total;
-	}
 }
